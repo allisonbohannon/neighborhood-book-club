fb9977c05c9342189743b08825c790967233a0cd
Allison Bohannon <akboh.github@gmail.com>
2023-04-12 10:33:16 -0700

    Generate user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do 
+   User.create(
+        username: Faker::Superhero.unique.name,
+        password_digest: BCrypt::Password.create('password'),
+        first_name: Faker::Name.unique.first_name, 
+        last_name: Faker::Name.unique.last_name,
+        bio: Faker::Quote.jack_handey,
+        zipcode: Faker::Address.zip,
+    )
+end
+
+puts "done seeding!"

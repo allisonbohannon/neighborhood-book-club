12b0e4df0bdc4c62c09df627f2af7ed75837bee7
Allison Bohannon <akboh.github@gmail.com>
2023-04-12 19:58:31 -0700

    Update sessions to persist login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
+        byebug
         session.delete :user_id 
         head :no_content 
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,42 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users, include: ['reading_lists', 'reading_lists.book']
+    users = User.all
+    render json: users, include: ['reading_lists', 'reading_lists.book']
   end
 
-  # GET /users/1
-  def show
-    user = User.find_by(id: session[:user_id])
-    render json: user
-  end
-
-  # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
     else
-      render json: @user.errors.full_messages, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+  def show 
+      user = find_user
+      render json: user
   end
 
-  # DELETE /users/1
   def destroy
-    @user.destroy
+      user = find_user
+      user.destroy
+      head :no_content
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+
+private 
+
+  def find_user
+      User.find_by(id: session[:user_id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :first_name, :last_name, :email, :password_digest, :bio, :zipcode)
-    end
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :password_digest, :bio, :zipcode)
+  end
+
 end

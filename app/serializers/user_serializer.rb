d9975ba01e0cfbb63f87cf72ac94a85c2300afc2
Allison Bohannon <akboh.github@gmail.com>
2023-04-22 10:25:38 -0700

    Update shown data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :email, :password_digest, :bio, :zipcode, :follower_count, :following_count, :books_read
 
   has_many :reading_lists, serializer: UserReadingListSerializer
+  has_many :book_club_members, serializer: UserBookClubSerializer
   has_many :follower_relationships, serializer: FollowerRelationshipSerializer
   has_many :following_relationships, serializer: FollowingRelationshipSerializer
 end

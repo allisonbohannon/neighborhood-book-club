aa305e3b2cf361e301c89eaa3baa41aea6a8b83a
Allison Bohannon <akboh.github@gmail.com>
2023-04-22 20:22:04 -0700

    Straighten out follower/following logic

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -15,11 +15,11 @@ const Profile = ({onUpdateUser}) => {
     const [edit, setEdit] = useState(false)
     const [display, setDisplay] = useState('')
 
-    const displayFollowers = currentUser.following_relationships.map(follower => {
+    const displayFollowing = currentUser.following_relationships.map(follower => {
         return (<li key={follower.id} style={{listStyle:'none'}}><UserCard user={follower.follower} onUpdateUser={onUpdateUser}/> </li>)
     }); 
 
-    const displayFollowing = currentUser.follower_relationships.map(following => {
+    const displayFollowers = currentUser.follower_relationships.map(following => {
         return (<li key={following.id} style={{listStyle:'none'}}><UserCard user={following.following} onUpdateUser={onUpdateUser}/></li>)
     })
 
@@ -29,15 +29,15 @@ const Profile = ({onUpdateUser}) => {
             {edit === false? <ProfileData setEdit={setEdit} /> : <ProfileEdit onUpdateUser={onUpdateUser} setEdit={setEdit} />}
             <br></br>
             <span style={{display:'flex', justifyContent:'space-evenly'}}>
-                <Button onClick={(e) => setDisplay(e.target.value)} value="Followers">Followers: {currentUser.following_count}</Button>
+                <Button onClick={(e) => setDisplay(e.target.value)} value="Following">Following: {currentUser.following_count}</Button>
                 <Typography> | </Typography>
-                <Button onClick={(e) => setDisplay(e.target.value)} value="Following">Following: {currentUser.follower_count}</Button>
+                <Button onClick={(e) => setDisplay(e.target.value)} value="Followers">Followers: {currentUser.follower_count}</Button>
             </span>
             <Grid item xs={8} gutter textAlign={'center'} >
                 <Typography variant="h4" >{display}</Typography>
                 <br></br>
                 {display === "Followers"? displayFollowers : ''}
-                {display === "Followings"? displayFollowing : ''}
+                {display === "Following"? displayFollowing : ''}
             </Grid>
     </Container>
     )

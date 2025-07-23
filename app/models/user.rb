1a8771280fb6b0c4e0f03ee66a0f9c948fd8947e
Allison Bohannon <akboh.github@gmail.com>
2025-07-23 14:56:21 -0700

    Update zipcode validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,17 +16,17 @@ class User < ApplicationRecord
             dependent:   :destroy
     has_many :followings,  through: :following_relationships, source: :following
 
-    def valid_zipcodes
-        Zipcode.map {|z| z.zip}
-    end
+   # def valid_zipcodes
+       # Zipcode.map {|z| z.zip}
+    #end
 
     validates :username, :email, :zipcode, presence: true
     validates :username, uniqueness: { message: "%{value} is already taken"}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false, message: "There is already an account associated with this email"}
     validates :password, length: { minimum: 8 }, if: :password_digest_changed?
-    validates :zipcode,  inclusion: { in: Zipcode.all.map {|z| z.zip} , 
-        message: "%{value} is not a valid US zipcode" }
+   # validates :zipcode,  inclusion: { in: Zipcode.all.map {|z| z.zip} , 
+    #    message: "%{value} is not a valid US zipcode" }
     validates :bio, length: { maximum: 500, 
          too_long: "%{count} characters is the maximum allowed"}
 
